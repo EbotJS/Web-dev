8f05fa4e48e440110e4a24d8d181bac61bb7a367
EbotProg <achaleebotoma2002@gmail.com>
2022-03-09 03:16:39 +0100

    followed up some coding videos

---
+++ Modern Javascript tutorials/chapter2/sandbox.js
@@ -0,0 +1,64 @@
+// // strings
+// console.log('hello, world');
+
+// let email = 'achaleebotoma2002@gmail.com';
+// console.log(email);
+
+
+// // string concatenation
+// let firstName = 'Achale';
+// let lastName = 'Oma';
+
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+
+// // string characters
+
+// console.log(firstName[1]);
+
+// //string length
+
+// console.log(firstName.length);
+// //string methods
+
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result);
+// console.log(fullName);
+
+// let index = email.indexOf('@');
+// console.log(index);
+
+
+
+//  common string methods
+let email = 'achaleebotoma2002@gmail.com';
+// let result = email.lastIndexOf('e');
+let result = email.slice(0, 12);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
